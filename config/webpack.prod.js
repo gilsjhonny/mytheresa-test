9f8c561a24a0d893667952f0c2eb00ab5544507e
Jhonny Gil <gilsjhonny@gmail.com>
2021-06-20 15:13:21 -0300

    Add webpack prod config and optimize css

---
+++ config/webpack.prod.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: ["./src/index.js"],
+  },
+  mode: "production",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // Translates CSS into CommonJS
+          "sass-loader", // Compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new OptimizeCssAssetsPlugin(), // Optimize css bundle: Remove duplicated code...
+    new MiniCssExtractPlugin({
+      filename: "[name]-[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
