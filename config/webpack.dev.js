2a053a5f8d94d62c188dc627ef7505a953288907
Jhonny Gil <gilsjhonny@gmail.com>
2021-06-20 10:13:28 -0300

    Add webpack, babel and react (minimal setup)

---
+++ config/webpack.dev.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: ["./src/main.js"],
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+  },
+  devServer: {
+    contentBase: "dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+  ],
+};
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>The Movies</div>, document.getElementById("root"));
